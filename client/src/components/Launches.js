10e595c8f579a777f549000bf087968fc23dd196
Jonathan Hsu <bit172@gmail.com>
2019-09-21 01:03:06 -0400

    New React Fragment Syntax

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import LaunchItem from "./LaunchItem";
@@ -17,7 +17,7 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
   render() {
     return (
-      <Fragment>
+      <>
         <h1 className="display-4 my-3">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
@@ -25,15 +25,15 @@ export class Launches extends Component {
             if (error) console.log(error);
             console.log(data);
             return (
-              <Fragment>
+              <>
                 {data.launches.map(launch => (
                   <LaunchItem key={launch.mission_name} launch={launch} />
                 ))}
-              </Fragment>
+              </>
             );
           }}
         </Query>
-      </Fragment>
+      </>
     );
   }
 }
